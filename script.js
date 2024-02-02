0b394f25c7888d58f7c56a5e4391857fb0d69f08
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-02 12:43:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const confirm = confirm("Do you want to proceed?")
+if(confirm){
+	alert("You clicked OK. Proceeding...")
+}else{
+	alert("You clicked Cancel. Exiting...")
+}
